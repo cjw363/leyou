55625cc10d86898c04c840f653687590581793de
cjw <771613512@qq.com>
2019-07-08 22:03:55 +0800

    提交

--- leyou-search/src/main/java/com/leyou/search/service/SearchService.java
+++ leyou-search/src/main/java/com/leyou/search/service/SearchService.java
@@ -3,6 +3,7 @@ package com.leyou.search.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.leyou.item.pojo.Brand;
+import com.leyou.item.pojo.PageResult;
 import com.leyou.item.pojo.Sku;
 import com.leyou.item.pojo.SpecParam;
 import com.leyou.item.pojo.Spu;
@@ -12,10 +13,18 @@ import com.leyou.search.client.CategoryClient;
 import com.leyou.search.client.GoodsClient;
 import com.leyou.search.client.SpecificationClient;
 import com.leyou.search.pojo.Goods;
+import com.leyou.search.pojo.SearchRequest;
+import com.leyou.search.reponsitory.GoodsRepository;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.query.FetchSourceFilter;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -140,4 +149,36 @@ public class SearchService {
         return result;
     }
 
+    @Autowired
+    private GoodsRepository goodsRepository;
+
+    public PageResult<Goods> search(SearchRequest request) {
+        String key = request.getKey();
+        // 判断是否有搜索条件，如果没有，直接返回null。不允许搜索全部商品
+        if (StringUtils.isBlank(key)) {
+            return null;
+        }
+
+        // 构建查询条件
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+
+        // 1、对key进行全文检索查询
+        queryBuilder.withQuery(QueryBuilders.matchQuery("all", key).operator(Operator.AND));
+
+        // 2、通过sourceFilter设置返回的结果字段,我们只需要id、skus、subTitle
+        queryBuilder.withSourceFilter(new FetchSourceFilter(
+          new String[]{"id","skus","subTitle"}, null));
+
+        // 3、分页
+        // 准备分页参数
+        int page = request.getPage();
+        int size = request.getSize();
+        queryBuilder.withPageable(PageRequest.of(page - 1, size));
+
+        // 4、查询，获取结果
+        Page<Goods> pageInfo = this.goodsRepository.search(queryBuilder.build());
+
+        // 封装结果并返回
+        return new PageResult<Goods>(pageInfo.getTotalElements(), (long)pageInfo.getTotalPages(), pageInfo.getContent());
+    }
 }
