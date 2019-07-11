50bc7a8c3bc593859abeb3b272aa0f1598c8090e
cjw363 <asd6555363>
2019-07-11 16:47:55 +0800

    提交

---
+++ leyou-sms/src/main/java/com/leyou/sms/LeyouSmsApplication.java
@@ -0,0 +1,17 @@
+package com.leyou.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Classname LeyouSmsApplication
+ * @Description
+ * @Date 2019/7/11 11:21
+ * @Created by cjw
+ */
+@SpringBootApplication
+public class LeyouSmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouSmsApplication.class, args);
+    }
+}
